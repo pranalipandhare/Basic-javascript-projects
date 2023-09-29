7ce2719ac6ff4d48e929183c31ab720052253db0
Pranali Pandhare <pranalipandhare2002@gmail.com>
2023-09-29 20:43:42 +0530

    commit

---
+++ #3 Pig Game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// selecting elements
+const score0 = document.querySelector('#score---0');
+const score1 = document.getElementById('score-1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score0El.textConetnt = 0;
+diceEl.classList.add('hidden');
